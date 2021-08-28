7321f61d5d718bf5ff518d74e5e7d7e63c7eb97f
Haja Rasoahaingo <haja.rasoah@gmail.com>
2021-08-29 00:05:47 +0200

    init

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <span>Hello.</span>
+        <style jsx>{` span { color: red; } `}</style>
+    </div>
+);
+
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root),
+
+ReactDOM.render(
+    <App />,
+    root
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+      rules: [
+          {
+              test: /\.(tsx)$/,
+              exclude: [/node_modules/],
+              use: [
+                  { loader: 'babel-loader' },
+                  { loader: 'ts-loader' }
+              ]
+          }
+      ]
+  },
+};
